8e43b62a3ee5716dd79d1051a345440044adf4ba
Cadu <dudu.spgomes@gmail.com>
2022-02-15 15:15:38 -0300

    refactor: endpoint max param

--- src/repository/get-rand-value.js
+++ src/repository/get-rand-value.js
@@ -1,5 +1,5 @@
 const endpoint =
-  "https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=5";
+  "https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300";
 
 export async function getRandValue() {
   const resp = await fetch(endpoint);
