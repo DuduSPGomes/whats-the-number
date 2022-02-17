28da1eca557c63a14f1a466bdd72175e755c5e15
Cadu <dudu.spgomes@gmail.com>
2022-02-17 01:55:46 -0300

    feat: jest config transforms

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   roots: ["src/__tests__"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.css$": "jest-transform-css",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "jest-transform-file",
+  },
 };
